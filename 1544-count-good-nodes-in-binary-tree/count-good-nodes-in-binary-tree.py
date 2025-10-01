2cfca5d8e687e876cc50b12032b6b2bba8248a64
Gordon Wai Hin Kam <157230866+li23179@users.noreply.github.com>
2025-10-01 11:28:10 +0100

    Time: 156 ms (22.14%) | Memory: 32.2 MB (33.10%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -4,18 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Good node (at node X) => the path from root to X, 
+#                          X must be >= to all path values
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         
-        def dfs(curr, max_val):
-
-            if not curr:
+        def dfs(node, max_val):
+            
+            if not node:
                 return 0
 
-            good = 1 if curr.val >= max_val else 0
-            max_val = max(curr.val, max_val)
-            left = dfs(curr.left, max_val)
-            right = dfs(curr.right, max_val)
+            # logic to decide good nodes 
+            good = 1 if node.val >= max_val else 0
+            # update the max_val for its children since we have
+            # encounter a new val (i.e. current node)
+            max_val = max(node.val, max_val)
+            # recursively call on left and right subtree
+            left = dfs(node.left, max_val)
+            right = dfs(node.right, max_val)
+            # add up all the good nodes on left and right subtree
+            # and also the current node
             return good + left + right
 
         return dfs(root, root.val)
+        
+        
