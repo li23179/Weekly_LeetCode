d6b53b82aad2e01a5c7889032d8949774979cc71
Gordon Wai Hin Kam <157230866+li23179@users.noreply.github.com>
2025-10-05 19:10:16 +0100

    Time: 7 ms (50.81%) | Memory: 21.2 MB (81.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,45 +1,30 @@
 class Solution:
+
+    # O(N + klog n) time complexity => build up freq table + heapify k times 
+    # O(N) space => 
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        # Example : [1, 2, 2, 3, 3, 3, 4, 4, 4], k = 3
-        # Output : [4, 3, 2]
-        # Bucket sort works in this case because 1 <= k <= len(nums)
-        
-        # idea:
-        # Create a list of bucket that store the num in nums list
-        # it's index is the frequency 
-        # e.g. at index 3, we have [3, 4] since 3 and 4 have a freq of 3
-        # We pop the bucket
-
-        # we can have upto len(nums) frequent element
-        # e.g input can be [5,5,5,5,5] k = 5
-        buckets = [[] for _ in range(len(nums) + 1)]
         nums_dict = {}
-        res = []
 
-        # build up the frequency table {num : freq} => 
-        # {1: 1, 2: 2, 3: 3}
+        # similar to bucket we need to store the freq of each num in nums
         for num in nums:
             nums_dict[num] = nums_dict.get(num, 0) + 1
 
-        # [[1], [2], [3, 4]]
-        for num, freq in nums_dict.items():
-            buckets[freq].append(num)
+        # Create a max_heap 
+        # in python, heapq lib implements as a min-heap
+        # we can create a put a negative sign in order to create a max heap
+        # heapify uses the first element of each node so we need to put
+        # frequency as first element 
 
-        # start from the last bucket since this is the most frequent 
-        # loop to find whether we have element that is correspond to this
-        # frequency, pop it and append it into our res
+        max_heap = [(-freq, num) for num, freq in nums_dict.items()]
+        heapq.heapify(max_heap)
 
-        freq = len(nums)
-        while k > 0 and buckets:
-            # loop the buckets[freq] to pop the element in this bucket
-            # remember to decrement k
-            while k > 0 and buckets[freq]:
-                res.append(buckets[freq].pop())
-                k -= 1
-            # if there is not more element correspond to this freq
-            # then go to the lower freq
-            freq -= 1
-
+        res = []
+        # heappop will help you to reheapify each time when you pop
+        # an element from the heap
+        for _ in range(k):
+            res.append(heapq.heappop(max_heap)[1])
+            
         return res
-        
+
