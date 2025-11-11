c7b097c77243334c0421a9e1e0db821682a5f5d4
Gordon Wai Hin Kam <157230866+li23179@users.noreply.github.com>
2025-11-11 00:33:54 +0000

    Time: 22 ms (84.01%) | Memory: 30.1 MB (32.93%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,7 +1,15 @@
 class Solution:
+    """
+    Floyd's Tortoise and Hare Algorithm
+    Uses fast and slow pointer
+    1. detect a cycle
+    2. find the start point of the cycle
+    There is a prove in the directory, have a look :)
+    """
     def findDuplicate(self, nums: List[int]) -> int:
         slow, fast = 0, 0
 
+        # first pass: to detect the cycle of a linked list
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
@@ -9,6 +17,8 @@ class Solution:
             if slow == fast:
                 break
 
+        # second pass: find where is the intersection 
+        # (i.e. start point of the cycle)
         slow2 = 0
         while True:
             slow = nums[slow]
