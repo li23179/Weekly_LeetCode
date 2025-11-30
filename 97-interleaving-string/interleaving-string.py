604ee14fa18dba86f80a372c4046c2cad680858d
Gordon Wai Hin Kam <157230866+li23179@users.noreply.github.com>
2025-11-30 14:48:09 +0000

    Time: 63 ms (5.51%) | Memory: 18.1 MB (56.93%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -32,6 +32,21 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
         
-        return self.top_down(s1, s2, s3)
+        # bottom up:
+        # initialise the search grid
+        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        # the last index matches the s3 last char so make sense
+        dp[len(s1)][len(s2)] = True
+
+        for i in range(len(s1) , -1, -1):
+            for j in range(len(s2), -1, -1):
+                
+                if i < len(s1) and s1[i] == s3[i+j] and dp[i+1][j]:
+                    dp[i][j] = True
+                
+                if j < len(s2) and s2[j] == s3[i+j] and dp[i][j+1]:
+                    dp[i][j] = True
+                
+        return dp[0][0]
 
             
