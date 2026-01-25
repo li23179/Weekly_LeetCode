4a3dafa7beffa287929fa88829964b7d598ba51e
Gordon Wai Hin Kam <157230866+li23179@users.noreply.github.com>
2026-01-25 12:31:12 +0000

    Time: 5 ms (51.89%) | Memory: 19.6 MB (10.37%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,28 +1,20 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        """
-        MMMDCCXLIX
-              i 
-        """
-        romans = [
-            ("M", 1000), ("CM", 900),
-            ("D", 500) , ("CD", 400),
-            ("C", 100) , ("XC", 90) ,
-            ("L", 50)  , ("XL", 40) ,
-            ("X", 10)  , ("IX", 9)  ,
-            ("V", 5)   , ("IV", 4)  ,
-            ("I", 1)
-        ]
+        symList = [(1000, "M"), (900, "CM"),
+                   (500, "D"), (400, "CD"),
+                   (100, "C"), (90, "XC"),
+                   (50, "L"), (40, "XL"),
+                   (10, "X"), (9, "IX"),
+                   (5, "V"), (4, "IV"),
+                   (1, "I")]
 
         res = ""
 
-        for roman, val in romans:
+        for val, sym in symList:
             if num // val:
                 count = num // val
-                res += (roman * count)
-                num = num % val
+                res += sym * count
+                num %= val
         
         return res
-
-
-
+            
